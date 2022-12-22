f6fa39e756f5732584c2e011953a3a90cb4eeefa
Trong Nhan Nguyen <trong.nguyen@tq933wj62l.ad.deliveryhero.com>
2022-12-22 10:46:49 +0100

    fix story text align

--- src/components/story.js
+++ src/components/story.js
@@ -8,9 +8,9 @@ const Story = () => {
     <section className="story relative">
       <div className=" storyDiv w-full h-screen /md:mt-20 brightness-75">
       </div>
-      <div className=" absolute left-0  bottom-10 md:bottom-1/10 text-center text-white pb-20 ">
+      <div className=" absolute left-0  bottom-10 md:bottom-1/10 text-center text-white pb-20 w-full ">
       <p className="bold"> Welcome to Pandora Microsite</p>
-      <h1 className="  text-6xl lg:text-9xl my-10 md:mb-20 md:w-3/4 md:mx-auto	">
+      <h1 className="  text-6xl lg:text-9xl my-10 md:mb-20 md:w-3/4 md:mx-auto	max-w-1200">
         One platform for all consumers
       </h1>
       <Link  to="/#what"  className=" text-white bg-pink py-3  md:py-6 px-6 md:px-10 rounded-custom md:my-8 hover:text-green tracking-widest font-bold"> DISCOVER MORE </Link>
